e6597abb8d8281e4700c18ebec1a90b922f61fe5
WojtAcht <wachtelik@gmail.com>
2024-03-19 17:26:13 +0100

    feat: initialize poetry, move problem instances to a directory

--- GNBG_instances.py
+++ GNBG_instances.py
@@ -38,9 +38,27 @@ import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
+
 # Define the GNBG class
 class GNBG:
-    def __init__(self, MaxEvals, AcceptanceThreshold, Dimension, CompNum, MinCoordinate, MaxCoordinate, CompMinPos, CompSigma, CompH, Mu, Omega, Lambda, RotationMatrix, OptimumValue, OptimumPosition):
+    def __init__(
+        self,
+        MaxEvals,
+        AcceptanceThreshold,
+        Dimension,
+        CompNum,
+        MinCoordinate,
+        MaxCoordinate,
+        CompMinPos,
+        CompSigma,
+        CompH,
+        Mu,
+        Omega,
+        Lambda,
+        RotationMatrix,
+        OptimumValue,
+        OptimumPosition,
+    ):
         self.MaxEvals = MaxEvals
         self.AcceptanceThreshold = AcceptanceThreshold
         self.Dimension = Dimension
@@ -73,91 +91,126 @@ class GNBG:
                 else:
                     rotation_matrix = self.RotationMatrix
 
-                a = self.transform((x - self.CompMinPos[k, :].reshape(-1, 1)).T @ rotation_matrix.T, self.Mu[k, :], self.Omega[k, :])
-                b = self.transform(rotation_matrix @ (x - self.CompMinPos[k, :].reshape(-1, 1)), self.Mu[k, :], self.Omega[k, :])
-                f[k] = self.CompSigma[k] + (a @ np.diag(self.CompH[k, :]) @ b) ** self.Lambda[k]
+                a = self.transform(
+                    (x - self.CompMinPos[k, :].reshape(-1, 1)).T @ rotation_matrix.T,
+                    self.Mu[k, :],
+                    self.Omega[k, :],
+                )
+                b = self.transform(
+                    rotation_matrix @ (x - self.CompMinPos[k, :].reshape(-1, 1)),
+                    self.Mu[k, :],
+                    self.Omega[k, :],
+                )
+                f[k] = (
+                    self.CompSigma[k]
+                    + (a @ np.diag(self.CompH[k, :]) @ b) ** self.Lambda[k]
+                )
 
             result[jj] = np.min(f)
-            if self.FE > (self.MaxEvals-1):
+            if self.FE > (self.MaxEvals - 1):
                 return result
             self.FE += 1
             self.FEhistory = np.append(self.FEhistory, result[jj])
             if self.BestFoundResult > result[jj]:
                 self.BestFoundResult = result[jj]
-            if abs(self.FEhistory[self.FE-1] - self.OptimumValue) < self.AcceptanceThreshold and np.isinf(self.AcceptanceReachPoint):
+            if abs(
+                self.FEhistory[self.FE - 1] - self.OptimumValue
+            ) < self.AcceptanceThreshold and np.isinf(self.AcceptanceReachPoint):
                 self.AcceptanceReachPoint = self.FE
         return result
 
     def transform(self, X, Alpha, Beta):
         Y = X.copy()
-        tmp = (X > 0)
+        tmp = X > 0
         Y[tmp] = np.log(X[tmp])
-        Y[tmp] = np.exp(Y[tmp] + Alpha[0] * (np.sin(Beta[0] * Y[tmp]) + np.sin(Beta[1] * Y[tmp])))
-        tmp = (X < 0)
+        Y[tmp] = np.exp(
+            Y[tmp] + Alpha[0] * (np.sin(Beta[0] * Y[tmp]) + np.sin(Beta[1] * Y[tmp]))
+        )
+        tmp = X < 0
         Y[tmp] = np.log(-X[tmp])
-        Y[tmp] = -np.exp(Y[tmp] + Alpha[1] * (np.sin(Beta[2] * Y[tmp]) + np.sin(Beta[3] * Y[tmp])))
+        Y[tmp] = -np.exp(
+            Y[tmp] + Alpha[1] * (np.sin(Beta[2] * Y[tmp]) + np.sin(Beta[3] * Y[tmp]))
+        )
         return Y
 
+    @classmethod
+    def read_from_file(cls, problem_index: int) -> "GNBG":
+        assert 1 <= problem_index <= 24, "ProblemIndex must be between 1 and 24."
+        filename = f"problem_instances/f{ProblemIndex}.mat"
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        GNBG_tmp = loadmat(os.path.join(current_dir, filename))["GNBG"]
+        MaxEvals = np.array([item[0] for item in GNBG_tmp["MaxEvals"].flatten()])[0, 0]
+        AcceptanceThreshold = np.array(
+            [item[0] for item in GNBG_tmp["AcceptanceThreshold"].flatten()]
+        )[0, 0]
+        Dimension = np.array([item[0] for item in GNBG_tmp["Dimension"].flatten()])[
+            0, 0
+        ]
+        CompNum = np.array([item[0] for item in GNBG_tmp["o"].flatten()])[
+            0, 0
+        ]  # Number of components
+        MinCoordinate = np.array(
+            [item[0] for item in GNBG_tmp["MinCoordinate"].flatten()]
+        )[0, 0]
+        MaxCoordinate = np.array(
+            [item[0] for item in GNBG_tmp["MaxCoordinate"].flatten()]
+        )[0, 0]
+        CompMinPos = np.array(GNBG_tmp["Component_MinimumPosition"][0, 0])
+        CompSigma = np.array(GNBG_tmp["ComponentSigma"][0, 0], dtype=np.float64)
+        CompH = np.array(GNBG_tmp["Component_H"][0, 0])
+        Mu = np.array(GNBG_tmp["Mu"][0, 0])
+        Omega = np.array(GNBG_tmp["Omega"][0, 0])
+        Lambda = np.array(GNBG_tmp["lambda"][0, 0])
+        RotationMatrix = np.array(GNBG_tmp["RotationMatrix"][0, 0])
+        OptimumValue = np.array(
+            [item[0] for item in GNBG_tmp["OptimumValue"].flatten()]
+        )[0, 0]
+        OptimumPosition = np.array(GNBG_tmp["OptimumPosition"][0, 0])
+        return GNBG(
+            MaxEvals,
+            AcceptanceThreshold,
+            Dimension,
+            CompNum,
+            MinCoordinate,
+            MaxCoordinate,
+            CompMinPos,
+            CompSigma,
+            CompH,
+            Mu,
+            Omega,
+            Lambda,
+            RotationMatrix,
+            OptimumValue,
+            OptimumPosition,
+        )
 
-# Get the current script's directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Define the path to the folder where you want to read/write files
-folder_path = os.path.join(current_dir)
-
-# Initialization
 ProblemIndex = 20  # Choose a problem instance range from f1 to f24
-
-# Preparation and loading of the GNBG parameters based on the chosen problem instance
-if 1 <= ProblemIndex <= 24:
-    filename = f'f{ProblemIndex}.mat'
-    GNBG_tmp = loadmat(os.path.join(folder_path, filename))['GNBG']
-    MaxEvals = np.array([item[0] for item in GNBG_tmp['MaxEvals'].flatten()])[0, 0]
-    AcceptanceThreshold = np.array([item[0] for item in GNBG_tmp['AcceptanceThreshold'].flatten()])[0, 0]
-    Dimension = np.array([item[0] for item in GNBG_tmp['Dimension'].flatten()])[0, 0]
-    CompNum = np.array([item[0] for item in GNBG_tmp['o'].flatten()])[0, 0]  # Number of components
-    MinCoordinate = np.array([item[0] for item in GNBG_tmp['MinCoordinate'].flatten()])[0, 0]
-    MaxCoordinate = np.array([item[0] for item in GNBG_tmp['MaxCoordinate'].flatten()])[0, 0]
-    CompMinPos = np.array(GNBG_tmp['Component_MinimumPosition'][0, 0])
-    CompSigma = np.array(GNBG_tmp['ComponentSigma'][0, 0], dtype=np.float64)
-    CompH = np.array(GNBG_tmp['Component_H'][0, 0])
-    Mu = np.array(GNBG_tmp['Mu'][0, 0])
-    Omega = np.array(GNBG_tmp['Omega'][0, 0])
-    Lambda = np.array(GNBG_tmp['lambda'][0, 0])
-    RotationMatrix = np.array(GNBG_tmp['RotationMatrix'][0, 0])
-    OptimumValue = np.array([item[0] for item in GNBG_tmp['OptimumValue'].flatten()])[0, 0]
-    OptimumPosition = np.array(GNBG_tmp['OptimumPosition'][0, 0])
-else:
-    raise ValueError('ProblemIndex must be between 1 and 24.')
-
-gnbg = GNBG(MaxEvals, AcceptanceThreshold, Dimension, CompNum, MinCoordinate, MaxCoordinate, CompMinPos, CompSigma, CompH, Mu, Omega, Lambda, RotationMatrix, OptimumValue, OptimumPosition)
-
-# Set a random seed for the optimizer
 np.random.seed()  # This uses a system-based source to seed the random number generator
-
-
-#Your optimization algorithm goes here
+gnbg = GNBG.read_from_file(ProblemIndex)
 Number_of_Solutions = 10000
-X = np.random.rand(Number_of_Solutions, Dimension) # This is for generating a random population of a number of solutions for testing GNBG
+X = np.random.rand(
+    Number_of_Solutions, gnbg.Dimension
+)  # This is for generating a random population of a number of solutions for testing GNBG
 # Calculating the fitness=objective values of the population using the GNBG function. The result is a Number_of_Solutions*1 vector of objective values
-result = gnbg.fitness(X) 
+result = gnbg.fitness(X)
 
 # After running the algorithm, the best fitness value is stored in gnbg.BestFoundResult
 # The function evaluation number where the algorithm reached the acceptance threshold is stored in gnbg.AcceptanceReachPoint
 # For visualizing the convergence behavior, the history of the objective values is stored in gnbg.FEhistory, however it needs to be processed as follows:
 
 convergence = []
-best_error = float('inf')
+best_error = float("inf")
 for value in gnbg.FEhistory:
-    error = abs(value - OptimumValue)
+    error = abs(value - gnbg.OptimumValue)
     if error < best_error:
         best_error = error
     convergence.append(best_error)
 
 # Plotting the convergence
 plt.plot(range(1, len(convergence) + 1), convergence)
-plt.xlabel('Function Evaluation Number (FE)')
-plt.ylabel('Error')
-plt.title('Convergence Plot')
-#plt.yscale('log')  # Set y-axis to logarithmic scale
+plt.xlabel("Function Evaluation Number (FE)")
+plt.ylabel("Error")
+plt.title("Convergence Plot")
+# plt.yscale('log')  # Set y-axis to logarithmic scale
 plt.show()
