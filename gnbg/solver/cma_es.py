4683c7b181c55bd6b9b8124b2263242a8889bb9d
WojtAcht <wachtelik@gmail.com>
2024-03-20 10:11:07 +0100

    feat: use BIPOP cma-es, add CMAESConfig

--- gnbg/solver/cma_es.py
+++ gnbg/solver/cma_es.py
@@ -1,13 +1,37 @@
 import random
+from typing import TypedDict
 
 import numpy as np
-from cma import CMAEvolutionStrategy
+from cma import fmin
 
 from ..problem_instance import GNBG
 from .solver import Solution, Solver
 
+CONFIG_FIELDS_OPTIONS = ["popsize", "tolfun"]
+
+
+class CMAESConfig(TypedDict, total=False):
+    popsize: int
+    tolfun: float
+    sigma0: float
+    incpopsize: int
+    restarts: int
+    restart_from_best: bool
+
+
+DEFAULT_CMAES_CONFIG = {
+    "sigma0": 1.0,
+    "incpopsize": 1,
+    "restarts": 10,
+    "restart_from_best": True,
+}
+
 
 class CMAESSolver(Solver):
+    def __init__(self, config: CMAESConfig = {}):
+        # Prepopulate the config with default values:
+        self.config = DEFAULT_CMAES_CONFIG | config
+
     def __call__(
         self,
         problem: GNBG,
@@ -18,16 +42,22 @@ class CMAESSolver(Solver):
             np.random.seed(random_state)
             random.seed(random_state)
         x0 = np.random.uniform(problem.MinCoordinate, problem.MaxCoordinate, problem.Dimension)
-        sigma = 1.0
-        cma = CMAEvolutionStrategy(
+        options = {
+            "bounds": [problem.MinCoordinate, problem.MaxCoordinate],
+            "verbose": -9,
+            "maxfevals": max_n_evals,
+            "seed": random_state,
+        }
+        options_from_config = {key: value for key, value in self.config.items() if key in CONFIG_FIELDS_OPTIONS}
+        options |= options_from_config
+        res = fmin(
+            problem,
             x0,
-            sigma,
-            inopts={
-                "bounds": [problem.MinCoordinate, problem.MaxCoordinate],
-                "verbose": -9,
-                "seed": random_state,
-                "maxfevals": max_n_evals,
-            },
+            sigma0=self.config["sigma0"],
+            options=options,
+            bipop=True,
+            restart_from_best=self.config["restart_from_best"],
+            restarts=self.config["restarts"],
+            incpopsize=self.config["incpopsize"],
         )
-        cma.optimize(problem)
-        return Solution(cma.result.xbest, cma.result.fbest, problem)
+        return Solution(res[0], res[1], problem)
